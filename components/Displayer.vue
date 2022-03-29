c0ab07bd7838e22d0336fd6b38cdc9448ac4e348
Kirill Dogadin <kirillsc2@gmail.com>
2022-03-29 15:34:07 +0200

    have it all

---
+++ components/Displayer.vue
@@ -0,0 +1,12 @@
+<template>
+  <a>{{ mystr }}</a>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  mystr: {
+    type: String,
+    required: true,
+  },
+});
+</script>
---
+++ pages/[word].vue
@@ -0,0 +1,19 @@
+<template>
+    <Displayer :mystr="$route.params.word"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+
+        const route = useRoute();
+        const allowed_routes = ['/world', '/hello'];
+        if (!allowed_routes.includes(route.path)) {
+            navigateTo('/');
+        }
+    },
+})
+</script>
+
